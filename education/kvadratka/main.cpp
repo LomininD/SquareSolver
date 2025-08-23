a1f2c26cd98d23fae72ef5cfea4f3268ee19da38
LomininD <saimon1402@mail.ru>
2025-08-23 18:27:52 +0300

    Fixed bugs, added colored unit test output, added unit test messages even if they are succeeded

--- education/kvadratka/main.cpp
+++ education/kvadratka/main.cpp
@@ -29,13 +29,11 @@ bool is_zero(double n);
 bool check_buffer();
 void ask_for_new_equations(int* state);
 void test_solve_equation(void);
-void test(double a, double b, double c, root_number correct_number_of_roots, double correct_root_1, double correct_root_2);
+void test(double a, double b, double c, root_number correct_number_of_roots, double correct_root_1, double correct_root_2, int* succeeded, int* max_succeeded);
 
 
-// TODO: unit tests (done)
-// TODO: assert equal pointers (done)
-// TODO: sqrt (done)
-// TODO: bool + rename (done) + fixed assertion bugs
+//TODO: 3 files
+
 
 int main()
 {
@@ -48,7 +46,7 @@ int main()
 
     root_number number_of_roots = NO_ROOTS;
 
-    while (program_state == ON) // TODO: comparison (done)
+    while (program_state == ON)
     {
         read_koef(&a_koef, &b_koef, &c_koef);
 
@@ -69,7 +67,7 @@ void ask_for_new_equations(int* state)
 
     int ans = 0;
     int correct_input = scanf("%d", &ans);
-    while (correct_input != 1 || !check_buffer())
+    while (correct_input != 1 || !check_buffer() || (ans != 0 && ans != 1))
     {
         printf("Wrong input. Please try again:\n");
         clear_buffer();
@@ -123,7 +121,6 @@ void read_koef(double* a, double* b, double* c)
 
 void print_ans(root_number number_of_roots, double root_1, double root_2)
 {
-    // isfinite
     assert(isfinite(root_1));
     assert(isfinite(root_2));
 
@@ -251,7 +248,7 @@ bool is_equal(double a, double b)
 }
 
 
-void test(double a, double b, double c, root_number correct_number_of_roots, double correct_root_1, double correct_root_2)
+void test(double a, double b, double c, root_number correct_number_of_roots, double correct_root_1, double correct_root_2, int* succeeded, int* max_succeeded)
 {
     assert(isfinite(a));
     assert(isfinite(b));
@@ -261,23 +258,33 @@ void test(double a, double b, double c, root_number correct_number_of_roots, dou
 
     double root_1 = 0, root_2 = 0;
 
+    *max_succeeded += 1;
+
     root_number number_of_roots = solve_equation(a, b, c, &root_1, &root_2);
     if (!(number_of_roots == correct_number_of_roots && is_equal(root_1, correct_root_1) && is_equal(root_2, correct_root_2)))
-        printf("FAILED: solve_equation(%lf, %lf, %lf, &x1, &x2) --> roots: %d, x1 = %lf, x2 = %lf "
-        "(should be roots: %d, x1 = %lf, x2 = %lf\n)", a, b, c, number_of_roots, root_1, root_2, correct_number_of_roots, correct_root_1, correct_root_2);
+    {
+        printf("\033[1;31mFAILED\033[0m: solve_equation(%lf, %lf, %lf, &x1, &x2) --> roots: %d, x1 = %lf, x2 = %lf "
+        "\n(should be roots: %d, x1 = %lf, x2 = %lf)\n", a, b, c, number_of_roots, root_1, root_2, correct_number_of_roots, correct_root_1, correct_root_2);
+    }
+    else
+        printf("\033[1;32mOK\033[0m\n");
+        *succeeded += 1;
 }
 
 
 void test_solve_equation(void)
 {
-    test(1, 2, 1, ONE_ROOT, -1, 0);
-    test(0, 0, 1, NO_ROOTS, 0, 0);
-    test(1, -5, 6, TWO_ROOTS, 2, 3);
-    test(0, 1, 2, ONE_ROOT, -2, 0);
-    test(0, 0, 0, INF, 0, 0);
-    test(1, 0, 0, ONE_ROOT, 0, 0);
-    test(5, 0, 0, ONE_ROOT, 0, 0);
-    test(0, 1, 0, ONE_ROOT, 0, 0);
-    test(1, 1, 0, TWO_ROOTS, -1, 0);
+    int succeeded = 0, max_succeeded = 0;
 
+    test(1, 2, 1, ONE_ROOT, -1, 0, &succeeded ,&max_succeeded);
+    test(0, 0, 1, NO_ROOTS, 0, 0, &succeeded ,&max_succeeded);
+    test(1, -5, 6, TWO_ROOTS, 2, 3, &succeeded ,&max_succeeded);
+    test(0, 1, 2, ONE_ROOT, -2, 0, &succeeded ,&max_succeeded);
+    test(0, 0, 0, INF, 0, 0, &succeeded ,&max_succeeded);
+    test(1, 0, 0, ONE_ROOT, 0, 0, &succeeded ,&max_succeeded);
+    test(5, 0, 0, ONE_ROOT, 0, 0, &succeeded ,&max_succeeded);
+    test(0, 1, 0, ONE_ROOT, 0, 0, &succeeded ,&max_succeeded);
+    test(1, 1, 0, TWO_ROOTS, -1, 0, &succeeded ,&max_succeeded);
+
+    printf("\033[1mPassed %d/%d\033[0m\n", succeeded, max_succeeded);
 }
